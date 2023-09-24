fb2f1c308849f26052bb2f7bafbf9c77c41b64c3
Aditya <adityathebest2019@gmail.com>
2023-09-24 17:26:09 +0530

    server check

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const axios = require("axios");
 const ncs = require("nocopyrightsounds-api");
 const authRouter = require("./auth/auth");
 const userRouter = require("./routers/user");
@@ -13,7 +14,6 @@ app.use(
     cors({
         credentials: true,
         origin: process.env.CLIENT_URL,
-        
     })
 );
 app.use(cookieParser());
@@ -77,6 +77,51 @@ app.get("/songs/:genre", async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 8000;
+const cors = require("cors");
 
+const streamApp = express();
+streamApp.use(express.json());
+
+streamApp.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+    })
+);
+
+streamApp.get("/stream", async (req, res) => {
+    try {
+        const url = new URL(req.query?.url);
+
+        if (url) {
+            axios({
+                method: "get",
+                url: req.query.url,
+                responseType: "stream",
+            })
+                .then((response) => {
+                    const len = response.headers.get("content-length");
+                    const type = response.headers.get("content-type");
+                    if (len / 1024 / 1024 > 20) {
+                        return res.status(503).send("File too large");
+                    }
+                    res.writeHead(206, "", {
+                        "Accept-Ranges": "bytes",
+                        "Content-Length": len,
+                        "Content-Type": type,
+                        "Content-Range": `bytes 0-${len - 1}/${len}`,
+                    });
+                    response.data.pipe(res);
+                })
+                .catch((error) => {
+                    res.status(503).send("Error");
+                });
+        }
+    } catch (error) {
+        res.status(404).send("Error");
+    }
+});
+
+streamApp.listen(8080);
+
+const port = process.env.PORT || 8000;
 app.listen(port);
