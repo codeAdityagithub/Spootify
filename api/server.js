223cb35f103b9aae58fb7f5df53f08bc83bb2777
Aditya <adityathebest2019@gmail.com>
2023-09-29 19:38:48 +0530

    api error fix

--- api/server.js
+++ api/server.js
@@ -77,39 +77,39 @@ app.get("/songs/:genre", async (req, res) => {
     }
 });
 
-app.get("/stream", async (req, res) => {
-    try {
-        const url = new URL(req.query?.url);
+// app.get("/stream", async (req, res) => {
+//     try {
+//         const url = new URL(req.query?.url);
 
-        if (url) {
-            axios({
-                method: "get",
-                url: req.query.url,
-                responseType: "stream",
-            })
-                .then((response) => {
-                    const len = response.headers.get("content-length");
-                    const type = response.headers.get("content-type");
-                    if (len / 1024 / 1024 > 15) {
-                        return res.status(503).send("File too large");
-                    }
-                    res.writeHead(206, "", {
-                        "Accept-Ranges": "bytes",
-                        "Content-Length": len,
-                        "Content-Type": type,
-                        "Content-Range": `bytes 0-${len - 1}/${len}`,
-                    });
-                    response.data.pipe(res);
-                })
-                .catch((error) => {
-                    res.status(503).send("Server Error!");
-                });
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(404).send("Invalid Playback url");
-    }
-});
+//         if (url) {
+//             axios({
+//                 method: "get",
+//                 url: req.query.url,
+//                 responseType: "stream",
+//             })
+//                 .then((response) => {
+//                     const len = response.headers.get("content-length");
+//                     const type = response.headers.get("content-type");
+//                     if (len / 1024 / 1024 > 15) {
+//                         return res.status(503).send("File too large");
+//                     }
+//                     res.writeHead(206, "", {
+//                         "Accept-Ranges": "bytes",
+//                         "Content-Length": len,
+//                         "Content-Type": type,
+//                         "Content-Range": `bytes 0-${len - 1}/${len}`,
+//                     });
+//                     response.data.pipe(res);
+//                 })
+//                 .catch((error) => {
+//                     res.status(503).send("Server Error!");
+//                 });
+//         }
+//     } catch (error) {
+//         console.log(error);
+//         res.status(404).send("Invalid Playback url");
+//     }
+// });
 
 const PORT = process.env.PORT || 8000;
 connect().then(() => {
